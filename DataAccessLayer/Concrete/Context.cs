b3b56581a921a0c84cd3feb609d66f51cb5b2fb4
DESKTOP-56DC4TI\ersoy <ersoyyalp07@gmail.com>
2023-06-22 23:18:29 +0300

    denem commit

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -16,6 +16,7 @@ namespace DataAccessLayer.Concrete
         {
             optionsBuilder.UseSqlServer("server=DESKTOP-56DC4TI; database=Alp_BlogDb; integrated security=true;");
 
+            //dbcontextss
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
